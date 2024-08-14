0c306e85ba8ff602c6c0395279b306a898d40ab1
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-08-14 17:05:53 +0530

    quiz completed

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,11 +1,16 @@
 "use client";
 import questions from "@/lib/questions";
 import styles from "@/styles/quiz.module.scss";
+import stylesSum from "@/styles/summary.module.scss";
+import { motion } from "framer-motion";
 import { useState } from "react";
 
 const Quiz = () => {
   const [userAns, setUserAns] = useState([0]);
+
   const activeQuestionIndex = userAns.length;
+
+  const quizIsComplete = activeQuestionIndex === questions.length;
 
   const handleSelectAnswer = (selectedAns: string) => {
     // selectedAns shud be of type number
@@ -15,13 +20,28 @@ const Quiz = () => {
     });
   };
 
+  if (quizIsComplete) {
+    return (
+      <div id={stylesSum.summary}>
+        <motion.img
+          src="/quiz-complete.png"
+          alt="Trophy Icon"
+        />
+        <h2>Quiz Completed</h2>
+      </div>
+    );
+  }
+
+  const shuffledAns = [...questions[activeQuestionIndex].answers];
+  shuffledAns.sort(() => Math.random() - 0.5);
+
   return (
     <>
       <section id={styles.quiz}>
         <div id={styles.question}>
           <h2>{questions[activeQuestionIndex].text}</h2>
           <ul id={styles.answers}>
-            {questions[activeQuestionIndex].answers.map(answer => (
+            {shuffledAns.map(answer => (
               <li
                 key={answer}
                 className={styles.answer}
